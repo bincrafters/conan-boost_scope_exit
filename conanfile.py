5c73ef619c2a590080a56c266fbdd2d6df960c6a
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:20 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostScope_ExitConan(ConanFile):
     name = "Boost.Scope_Exit"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/scope_exit"
+    url = "https://github.com/bincrafters/conan-boost-scope_exit"
+    source_url = "https://github.com/boostorg/scope_exit"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "scope_exit"
@@ -20,7 +20,7 @@ class BoostScope_ExitConan(ConanFile):
 
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
