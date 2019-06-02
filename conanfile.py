9b66db1a30e9fd80376ec6e5e01224e221df37e6
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:08 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,18 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostScope_ExitConan(base.BoostBaseConan):
     name = "boost_scope_exit"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_scope_exit"
-    lib_short_names = ["scope_exit"]
-    header_only_libs = ["scope_exit"]
-    b2_requires = [
-        "boost_config",
-        "boost_function",
-        "boost_preprocessor",
-        "boost_type_traits",
-        "boost_typeof"
-    ]
+    version = "1.70.0"
